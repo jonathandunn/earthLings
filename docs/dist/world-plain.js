13032afc4453da5b9d32103828b23c2c804b1fd9
jonathandunn <jonathan.dunn@canterbury.ac.nz>
2019-08-17 10:04:22 +1200

    Update world-plain.js

--- docs/dist/world-plain.js
+++ docs/dist/world-plain.js
@@ -1,3 +1,3 @@
 var map = d3.geomap()
-    .geofile('https://raw.githubusercontent.com/jonathandunn/earthLings/master/docs/dist/topojson/world/countries.json')
+    .geofile('https://raw.githubusercontent.com/jonathandunn/earthlings/master/docs/dist/topojson/world/countries.json')
     .draw(d3.select('#map'));
