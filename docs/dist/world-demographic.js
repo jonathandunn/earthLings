78d0ed957de8d6a0415375e5480d390c029f5207
jonathandunn <jonathan.dunn@canterbury.ac.nz>
2019-08-17 22:35:36 +1200

    Update world-demographic.js

--- docs/dist/world-demographic.js
+++ docs/dist/world-demographic.js
@@ -1,6 +1,6 @@
 var map = d3.choropleth()
     .geofile('/earthLings/dist/topojson/world/countries.json')
-    .projection("d3.geoGilbert")
+    .projection("geoGilbert")
     .column('PCT_WORDS_CC4')
     .legend(true)
     .unitId('iso3');
