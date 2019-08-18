5f9e37a2696216f602850c0367de81e9b06eabc9
Jonathan Dunn <jonathan.dunn@canterbury.ac.nz>
2019-08-18 19:10:16 +1200

    Update world-major.js

--- docs/dist/world-major.js
+++ docs/dist/world-major.js
@@ -1,11 +1,11 @@
 // We define a variable holding the current key to visualize on the map.
 var currentColumn = 'N_CC4_eng';
-var currentType = "N_";
-var currentData = "CC4_";
-var currentLanguage = "eng";
+var currentType = 'N_';
+var currentData = 'CC4_';
+var currentLanguage = 'eng';
 
 // Listen to changes of the dropdown to select the key to visualize on the map.
-d3.select('#"select-type"').on('change', function(a) {
+d3.select('#select-type').on('change', function(a) {
   // Change the current key and call the function to update the colors.
   currentType = d3.select(this).property('value');
   currentColumn = currentType + currentData + currentLanguage;
@@ -14,7 +14,7 @@ d3.select('#"select-type"').on('change', function(a) {
 });
 
 // Listen to changes of the dropdown to select the key to visualize on the map.
-d3.select('#"select-data"').on('change', function(a) {
+d3.select('#select-data').on('change', function(a) {
   // Change the current key and call the function to update the colors.
   currentData = d3.select(this).property('value');
   currentColumn = currentType + currentData + currentLanguage;
@@ -23,7 +23,7 @@ d3.select('#"select-data"').on('change', function(a) {
 });
 
 // Listen to changes of the dropdown to select the key to visualize on the map.
-d3.select('#"select-language"').on('change', function(a) {
+d3.select('#select-language').on('change', function(a) {
   // Change the current key and call the function to update the colors.
   currentLanguage = d3.select(this).property('value');
   currentColumn = currentType + currentData + currentLanguage;
