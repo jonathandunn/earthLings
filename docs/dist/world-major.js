9c45fa8235b8bb77ece49d68e6858f27f3d44838
Jonathan Dunn <jonathan.dunn@canterbury.ac.nz>
2019-08-18 17:01:57 +1200

    Create world-major.js

---
+++ docs/dist/world-major.js
@@ -0,0 +1,21 @@
+// We define a variable holding the current key to visualize on the map.
+var currentColumn = 'PER_CAPITA_GDP';
+
+// Listen to changes of the dropdown to select the key to visualize on the map.
+d3.select('#select-key').on('change', function(a) {
+  // Change the current key and call the function to update the colors.
+  currentColumn = d3.select(this).property('value');
+  // Redo map
+  map.column(currentColumn).update()
+});
+
+var map = d3.choropleth()
+    .geofile('/earthLings/dist/topojson/world/countries.json')
+    .rotate([0,0,0])
+    .column(currentColumn)
+    .legend(true)
+    .unitId('iso3');
+
+d3.csv('/earthLings/data/map.countries.csv').then(data => {
+    map.draw(d3.select('#map').datum(data));
+});
