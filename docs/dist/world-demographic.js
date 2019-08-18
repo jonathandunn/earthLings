84905fa572d061e78adb43a1ebf0868d940c64ae
Jonathan Dunn <jonathan.dunn@canterbury.ac.nz>
2019-08-18 13:47:21 +1200

    Update world-demographic.js

--- docs/dist/world-demographic.js
+++ docs/dist/world-demographic.js
@@ -1,6 +1,18 @@
+// We define a variable holding the current key to visualize on the map.
+var currentColumn = 'PCT_WORDS_CC4';
+
+// Listen to changes of the dropdown to select the key to visualize on the map.
+d3.select('#select-key').on('change', function(a) {
+  // Change the current key and call the function to update the colors.
+  currentColumn = d3.select(this).property('value');
+  // Redo map
+  d3.csv('/earthLings/data/map.countries.csv').then(data => {
+    map.draw(d3.select('#map').datum(data));
+});
+
 var map = d3.choropleth()
     .geofile('/earthLings/dist/topojson/world/countries.json')
-    .column('PCT_WORDS_CC4')
+    .column(currentColumn)
     .legend(true)
     .unitId('iso3');
 
