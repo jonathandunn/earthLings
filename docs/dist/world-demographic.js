8029453e19d4e8b5490088c1dec1c42951e3cf5b
Jonathan Dunn <jonathan.dunn@canterbury.ac.nz>
2019-08-18 16:21:57 +1200

    Update world-demographic.js

--- docs/dist/world-demographic.js
+++ docs/dist/world-demographic.js
@@ -11,7 +11,6 @@ d3.select('#select-key').on('change', function(a) {
 
 var map = d3.choropleth()
     .geofile('/earthLings/dist/topojson/world/countries.json')
-    .height('600')
     .rotate([0,0,0])
     .column(currentColumn)
     .legend(true)
