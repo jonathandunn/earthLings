7bd0ff3dcd2219a07c64cd626aa4cc8bed43cc35
Jonathan Dunn <jonathan.dunn@canterbury.ac.nz>
2019-08-19 12:12:58 +1200

    Update world-demographic.js

--- docs/dist/world-demographic.js
+++ docs/dist/world-demographic.js
@@ -17,7 +17,7 @@ var map = d3.choropleth()
     .rotate([0,0,0])
     .column(currentColumn)
     .legend(true)
-    .valueScale()
+    .valueScale(d3.scaleQuantize)
     .unitId("iso3");
 
 d3.csv("/earthLings/data/map.countries.csv").then(data => {
