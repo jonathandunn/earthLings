c5d184f32c10b993d3b22b6475866bdef9132e9d
Jonathan Dunn <jonathan.dunn@canterbury.ac.nz>
2019-08-26 20:54:55 +1200

    Update world-demographic.js

--- docs/dist/world-demographic.js
+++ docs/dist/world-demographic.js
@@ -9,11 +9,30 @@ d3.select("#select-key").on("change", function(a) {
   map.column(currentColumn).update();
 });
 
+var format_small = function(d) {
+    return numberWithCommas(Math.round(d));
+}
+
+var format_mil = function(d) {
+    d = d / 1000000;
+    return d3.format(',.02f')(d) + 'M';
+}
+
+var format_per = function(d) {
+    d = d * 100;
+    return d3.format(',.01f')(d) + '%';
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 var map = d3.choropleth()
     .geofile("/dist/topojson/world/countries.json")
     .rotate([0,0,0])
     .column(currentColumn)
     .legend(true)
+    .format(format_small)
     .colors(['#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026'])
     .unitId("iso3");
 
