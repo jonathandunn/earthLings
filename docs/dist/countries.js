94427490581ebf17b0da08ed3dac9c2f41226ff2
Jonathan Dunn <jonathan.dunn@canterbury.ac.nz>
2019-09-02 11:35:41 +1200

    Update countries.js

--- docs/dist/countries.js
+++ docs/dist/countries.js
@@ -1,3 +1,13 @@
+// We define a variable holding the current key to visualize.
+var currentColumn = 'nzl';
+
+// Listen to changes of the dropdown to select the key to visualize.
+d3.select('#select-country').on('change', function(a) {
+  // Change the current key and call the function to update the colors.
+  currentType = d3.select(this).property('value');
+  currentColumn = currentType;
+}
+                                
 var margin = {top: 40, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
