0459d33ed8edae708e5b3104c884bcd923d9338e
Jonathan Dunn <jonathan.dunn@canterbury.ac.nz>
2020-04-07 09:33:20 +1200

    Update world-demographic.js

--- docs/dist/world-demographic.js
+++ docs/dist/world-demographic.js
@@ -21,9 +21,23 @@ var format_small = function(d) {
     return numberWithCommas(Math.round(d));
 }
 
-var format_mil = function(d) {
-    d = d;
-    return d3.format(',.00f')(d);
+var format_mil = function(Number) {
+    
+  // Nine Zeroes for Billions
+    return Math.abs(Number(labelValue)) >= 1.0e+9
+
+    ? Math.abs(Number(labelValue)) / 1.0e+9 + "B"
+    // Six Zeroes for Millions 
+    : Math.abs(Number(labelValue)) >= 1.0e+6
+
+    ? Math.abs(Number(labelValue)) / 1.0e+6 + "M"
+    // Three Zeroes for Thousands
+    : Math.abs(Number(labelValue)) >= 1.0e+3
+
+    ? Math.abs(Number(labelValue)) / 1.0e+3 + "K"
+
+    : Math.abs(Number(labelValue));
+    return d3.format(',.00f')(Number);
 }
 
 var format_per = function(d) {
