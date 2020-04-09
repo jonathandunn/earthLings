7cf9be1d10f34242185022fbabe61297e27886f9
Jonathan Dunn <jonathan.dunn@canterbury.ac.nz>
2020-04-10 11:38:23 +1200

    Update world-demographic.js

--- docs/dist/world-demographic.js
+++ docs/dist/world-demographic.js
@@ -1,6 +1,5 @@
 // We define a variable holding the current key to visualize on the map.
 var currentColumn = "Web Total Words Per Country";
-var domainVar = [];
 
 // Listen to changes of the dropdown to select the key to visualize on the map.
 d3.select("#select-key").on("change", function(a) {
@@ -51,7 +50,6 @@ var map = d3.choropleth()
     .column(currentColumn)
     .legend(true)
     .format(format_mil)
-    .domain(domainVar)
     .valueScale(d3.scaleQuantile)
     .colors(['#deedcf', '#cae5bb', '#b3dda6', '#99d492', '#7ecb7f', '#6ac276', '#56b870', '#43ae6c', '#30a46b', '#1d9a6c', '#198c75', '#147e7b', '#116270', '#0d4761', '#0c6051'])
     .unitId("iso3");
