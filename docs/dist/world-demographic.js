51b6f56763d17954334e54e7c39a6f2c6262b12c
Jonathan Dunn <jonathan.dunn@canterbury.ac.nz>
2020-04-10 09:36:41 +1200

    Update world-demographic.js

--- docs/dist/world-demographic.js
+++ docs/dist/world-demographic.js
@@ -51,7 +51,7 @@ var map = d3.choropleth()
     .legend(true)
     .format(format_mil)
     .valueScale(d3.scaleQuantile)
-    .colors(['#0a25f1', '#0d4761', '#116270', '#147e7b', '#198c75', '#1d9a6c', '#30a46b', '#43ae6c', '#56b870', '#6ac276', '#7ecb7f', '#99d492', '#b3dda6', '#cae5bb', '#deedcf'])
+    .colors(['#deedcf', '#cae5bb', '#b3dda6', '#99d492', '#7ecb7f', '#6ac276', '#56b870', '#43ae6c', '#30a46b', '#1d9a6c', '#198c75', '#147e7b', '#116270', '#0d4761', '#0a25f1'])
     .unitId("iso3");
 
 d3.csv("/data/map.countries.csv").then(data => {
