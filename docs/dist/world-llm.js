a7338a0ceee426384a226b8d6d3e63cbf8e65fc7
Jonathan Dunn <jonathan.dunn@canterbury.ac.nz>
2025-09-28 10:28:27 -0500

    Update world-llm.js

--- docs/dist/world-llm.js
+++ docs/dist/world-llm.js
@@ -11,31 +11,9 @@ d3.select('#select-key').on('change', function(a) {
   map.column(currentColumn).update()
 });
 
-var format_small = function(d) {
-    return numberWithCommas(Math.round(d));
-}
-
-var format_hhi = function(d) {
-    return (Math.round(d * 100) / 100).toFixed(2);
-}
-
-var format_mil = function(d) {
-    d = d;
-    return d3.format(',.00f')(d);
-}
-
 var format_dec = function(d) {
     d = d;
-    return (Math.round(d * 100) / 100).toFixed(2);
-}
-
-var format_per = function(d) {
-    d = d * 100;
-    return d3.format(',.01f')(d) + '%';
-}
-
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return (Math.round(d * 100) / 100).toFixed(3);
 }
 
 var map = d3.choropleth()
@@ -43,7 +21,7 @@ var map = d3.choropleth()
     .rotate([0,0,0])
     .column(currentColumn)
     .legend(true)
-    .format(format_per)
+    .format(format_dec)
     .valueScale(d3.scaleQuantile)
     .colors(['#deedcf', '#cae5bb', '#b3dda6', '#99d492', '#7ecb7f', '#6ac276', '#56b870', '#43ae6c', '#30a46b', '#1d9a6c', '#198c75', '#147e7b', '#116270', '#0d4761', '#0c6051'])
     .unitId("iso3");
